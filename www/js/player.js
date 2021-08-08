d76a40d9305a8f556d90292dc38e93b1f296878d
ElementalMP4 <seebeesbt@gmail.com>
2021-08-08 17:22:52 +0100

    feat(client): Implemented video loader system (#6)

--- www/js/player.js
+++ www/js/player.js
@@ -3,6 +3,42 @@ var Gateway = new WebSocket(GatewayServerURL);
 
 var USER_PROPERTIES;
 var TOKEN;
+var PLAYER;
+var CURRENT_VIDEO_ID;
+
+var PLAYER_READY = false;
+
+function onYouTubeIframeAPIReady() {
+    PLAYER = new YT.Player('player', {
+        height: '100%',
+        width: '80%',
+        videoId: CURRENT_VIDEO_ID,
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady() {
+    PLAYER_READY = true;
+}
+
+function onPlayerStateChange() {
+
+}
+
+function loadVideo(youTubeVideoID) {
+    CURRENT_VIDEO_ID = youTubeVideoID;
+    if (PLAYER_READY) PLAYER.loadVideoById(youTubeVideoID, 0);
+}
+
+function handleChatMessage() {
+
+}
 
 Gateway.onopen = function() {
     console.log("Connected To Gateway");
@@ -15,6 +51,14 @@ Gateway.onclose = function() {
 Gateway.onmessage = function(message) {
     const response = JSON.parse(message.data);
     console.log(response);
+
+    if (response.origin == "party-joinparty") loadVideo(JSON.parse(response.response).video);
+
+    switch (response.type) {
+        case "party-chatmessage":
+            handleChatMessage(response.data);
+            break;
+    }
 }
 
 function getToken() {
@@ -36,3 +80,11 @@ Gateway.onopen = function() {
         Gateway.send(JSON.stringify({ "type": "party-joinparty", "data": { "token": TOKEN, "roomID": selfURL.searchParams.get("roomID") } }));
     }
 }
+
+function embedPlayer() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+embedPlayer();
