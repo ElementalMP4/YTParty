4fb83e0bfb4ec119d070ad1fcb3e3b392c61086d
Seb <seebeesbt@gmail.com>
2022-01-19 13:05:53 +0000

    refac(api): Cleaned up HTTP rest controller

--- src/main/java/de/voidtech/ytparty/api/http/HttpRestController.java
+++ src/main/java/de/voidtech/ytparty/api/http/HttpRestController.java
@@ -9,6 +9,7 @@ import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,152 +33,162 @@ public class HttpRestController {
 	@Autowired
 	private PartyService partyService;
 	
-	@RequestMapping(value = "/", produces = "text/html")
-	public String indexRoute() {
-		return fileReader.getTextFileContents("html/index.html");
-	}
+	// # # # # Stylesheets # # # # 
 	
-	@RequestMapping(value = "/index.html", produces = "text/html")
-	public String indexHtmlRoute() {
-		return fileReader.getTextFileContents("html/index.html");
-	}
-	
-	@RequestMapping(value = "/terms.html", produces = "text/html")
-	public String termsRoute() {
-		return fileReader.getTextFileContents("html/terms.html");
-	}
-  
-	@RequestMapping(value = "/primarystyle.css", produces = "text/css")
+	@RequestMapping(value = "/primarystyle.css", produces = "text/css", method = RequestMethod.GET)
 	public String primaryStyleRoute() {
 		return fileReader.getTextFileContents("css/primarystyle.css");
 	}
 	
-	@RequestMapping(value = "/modal.css", produces = "text/css")
+	@RequestMapping(value = "/modal.css", produces = "text/css", method = RequestMethod.GET)
 	public String modalStyleRoute() {
 		return fileReader.getTextFileContents("css/modal.css");
 	}
 	
-	@RequestMapping(value = "/playerstyle.css", produces = "text/css")
+	@RequestMapping(value = "/playerstyle.css", produces = "text/css", method = RequestMethod.GET)
 	public String playerStyleRoute() {
 		return fileReader.getTextFileContents("css/playerstyle.css");
 	}
 	
-	@RequestMapping(value = "/modal.js", produces = "application/javascript")
-	public String modalScriptRoute() {
-		return fileReader.getTextFileContents("js/modal.js");
+	// # # # # HTML # # # #
+	
+	@RequestMapping(value = "/", produces = "text/html", method = RequestMethod.GET)
+	public String indexRoute() {
+		return fileReader.getTextFileContents("html/index.html");
 	}
 	
-	@RequestMapping(value = "/player.js", produces = "application/javascript")
-	public String playerScriptRoute() {
-		return fileReader.getTextFileContents("js/player.js");
+	@RequestMapping(value = "/index.html", produces = "text/html", method = RequestMethod.GET)
+	public String indexHtmlRoute() {
+		return fileReader.getTextFileContents("html/index.html");
 	}
 	
-	@RequestMapping(value = "/favicon.png", produces = "image/png")
-	public byte[] faviconRoute() {
-		return fileReader.getBinaryFileContents("img/favicon.png");
+	@RequestMapping(value = "/terms.html", produces = "text/html", method = RequestMethod.GET)
+	public String termsRoute() {
+		return fileReader.getTextFileContents("html/terms.html");
 	}
 	
-	@RequestMapping(value = "/favicon.ico", produces = "image/png")
-	public byte[] alternateFaviconRoute() {
-		return fileReader.getBinaryFileContents("img/favicon.png");
-	}
-	
-	@RequestMapping(value = "/waves.svg", produces = "image/svg+xml")
-	public byte[] wavesRoute() {
-		return fileReader.getBinaryFileContents("img/waves.svg");
-	}
-	
-	@RequestMapping(value = "/deadcat.gif", produces = {"image/gif"})
-	public byte[] catGifRoute() {
-		return fileReader.getBinaryFileContents("img/deadcat.gif");
-	}
-	
-	@RequestMapping(value = "/login.html", produces = "text/html")
+	@RequestMapping(value = "/login.html", produces = "text/html", method = RequestMethod.GET)
 	public String loginRoute() {
 		return fileReader.getTextFileContents("html/login.html");
 	}
 	
-	@RequestMapping(value = "/ping.html", produces = "text/html")
+	@RequestMapping(value = "/ping.html", produces = "text/html", method = RequestMethod.GET)
 	public String pingRoute() {
 		return fileReader.getTextFileContents("html/ping.html");
 	}
 	
-	@RequestMapping(value = "/signup.html", produces = "text/html")
+	@RequestMapping(value = "/signup.html", produces = "text/html", method = RequestMethod.GET)
 	public String signupRoute() {
 		return fileReader.getTextFileContents("html/signup.html");
 	}
 	
-	@RequestMapping(value = "/about.html", produces = "text/html")
+	@RequestMapping(value = "/about.html", produces = "text/html", method = RequestMethod.GET)
 	public String aboutRoute() {
 		return fileReader.getTextFileContents("html/about.html");
 	}
 	
-	@RequestMapping(value = "/signup.js", produces = "application/javascript")
-	public String signupScriptRoute() {
-		return fileReader.getTextFileContents("js/signup.js");
-	}
-
-	@RequestMapping(value = "/home.js", produces = "application/javascript")
-	public String homeScriptRoute() {
-		return fileReader.getTextFileContents("js/home.js");
-	}
-	
-	@RequestMapping(value = "/login.js", produces = "application/javascript")
-	public String signInScriptRoute() {
-		return fileReader.getTextFileContents("js/login.js");
-	}
-	
-	@RequestMapping(value = "/accountsettings.html", produces = "text/html")
+	@RequestMapping(value = "/accountsettings.html", produces = "text/html", method = RequestMethod.GET)
 	public String accountRoute() {
 		return fileReader.getTextFileContents("html/accountsettings.html");
 	}
 	
-	@RequestMapping(value = "/settings.js", produces = "application/javascript")
-	public String settingsScriptRoute() {
-		return fileReader.getTextFileContents("js/settings.js");
-	}
-	
-	@RequestMapping(value = "/home.html", produces = "text/html")
+	@RequestMapping(value = "/home.html", produces = "text/html", method = RequestMethod.GET)
 	public String homeRoute() {
 		return fileReader.getTextFileContents("html/home.html");
 	}
 	
-	@RequestMapping(value = "/createroom.js", produces = "application/javascript")
-	public String createRoomScriptRoute() {
-		return fileReader.getTextFileContents("js/createroom.js");
-	}
-	
-	@RequestMapping(value = "/createroom.html", produces = "text/html")
+	@RequestMapping(value = "/createroom.html", produces = "text/html", method = RequestMethod.GET)
 	public String createRoomRoute() {
 		return fileReader.getTextFileContents("html/createroom.html");
 	}
 	
-	@RequestMapping(value = "/doesonestillequalone.html", produces = "text/html")
+	@RequestMapping(value = "/doesonestillequalone.html", produces = "text/html", method = RequestMethod.GET)
 	public String easterEggRoute() {
 		return fileReader.getTextFileContents("/html/doesonestillequalone.html");
 	}
 	
-	@RequestMapping(value = "/resetpassword.html", produces = "text/html")
+	@RequestMapping(value = "/resetpassword.html", produces = "text/html", method = RequestMethod.GET)
 	public String passwordResetRoute() {
 		return fileReader.getTextFileContents("/html/resetpassword.html");
 	}
 	
-	@RequestMapping(value = "/forgotpassword.html", produces = "text/html")
+	@RequestMapping(value = "/forgotpassword.html", produces = "text/html", method = RequestMethod.GET)
 	public String forgotPasswordRoute() {
 		return fileReader.getTextFileContents("/html/forgotpassword.html");
 	}
 	
-	@RequestMapping(value = "/resetpassword.js", produces = "application/javascript")
+	// # # # # JavaScript # # # # 
+	
+	@RequestMapping(value = "/createroom.js", produces = "application/javascript", method = RequestMethod.GET)
+	public String createRoomScriptRoute() {
+		return fileReader.getTextFileContents("js/createroom.js");
+	}
+	
+	@RequestMapping(value = "/settings.js", produces = "application/javascript", method = RequestMethod.GET)
+	public String settingsScriptRoute() {
+		return fileReader.getTextFileContents("js/settings.js");
+	}
+	
+	@RequestMapping(value = "/resetpassword.js", produces = "application/javascript", method = RequestMethod.GET)
 	public String passwordResetScriptRoute() {
 		return fileReader.getTextFileContents("/js/resetpassword.js");
 	}
 	
-	@RequestMapping(value = "/forgotpassword.js", produces = "application/javascript")
+	@RequestMapping(value = "/forgotpassword.js", produces = "application/javascript", method = RequestMethod.GET)
 	public String forgotPasswordScriptRoute() {
 		return fileReader.getTextFileContents("/js/forgotpassword.js");
 	}
 	
-	@RequestMapping(value = "/particle-config.json")
+	@RequestMapping(value = "/modal.js", produces = "application/javascript", method = RequestMethod.GET)
+	public String modalScriptRoute() {
+		return fileReader.getTextFileContents("js/modal.js");
+	}
+	
+	@RequestMapping(value = "/player.js", produces = "application/javascript", method = RequestMethod.GET)
+	public String playerScriptRoute() {
+		return fileReader.getTextFileContents("js/player.js");
+	}
+	
+	@RequestMapping(value = "/signup.js", produces = "application/javascript", method = RequestMethod.GET)
+	public String signupScriptRoute() {
+		return fileReader.getTextFileContents("js/signup.js");
+	}
+
+	@RequestMapping(value = "/home.js", produces = "application/javascript", method = RequestMethod.GET)
+	public String homeScriptRoute() {
+		return fileReader.getTextFileContents("js/home.js");
+	}
+	
+	@RequestMapping(value = "/login.js", produces = "application/javascript", method = RequestMethod.GET)
+	public String signInScriptRoute() {
+		return fileReader.getTextFileContents("js/login.js");
+	}
+	
+	// # # # # Images # # # # 
+	
+	@RequestMapping(value = "/favicon.png", produces = "image/png", method = RequestMethod.GET)
+	public byte[] faviconRoute() {
+		return fileReader.getBinaryFileContents("img/favicon.png");
+	}
+	
+	@RequestMapping(value = "/favicon.ico", produces = "image/png", method = RequestMethod.GET)
+	public byte[] alternateFaviconRoute() {
+		return fileReader.getBinaryFileContents("img/favicon.png");
+	}
+	
+	@RequestMapping(value = "/waves.svg", produces = "image/svg+xml", method = RequestMethod.GET)
+	public byte[] wavesRoute() {
+		return fileReader.getBinaryFileContents("img/waves.svg");
+	}
+	
+	@RequestMapping(value = "/deadcat.gif", produces = "image/gif", method = RequestMethod.GET)
+	public byte[] catGifRoute() {
+		return fileReader.getBinaryFileContents("img/deadcat.gif");
+	}
+	
+	// # # # # Miscellaneous # # # # 
+	
+	@RequestMapping(value = "/particle-config.json", produces = "text/json", method = RequestMethod.GET)
 	public String particleConfigRoute() {
 		String mode = configService.getParticleMode();
 		return fileReader.getTextFileContents("/particle-config-" + mode + ".json");
@@ -190,7 +201,7 @@ public class HttpRestController {
 		return partyDoc.toString();
 	}
 	
-	@RequestMapping(value = "/player.html", produces = "text/html")
+	@RequestMapping(value = "/player.html", produces = "text/html", method = RequestMethod.GET)
 	public String playerRoute(@RequestParam(required = false) String roomID) {
 		if (roomID == null) return fileReader.getTextFileContents("html/noroom.html");
 		else {
@@ -200,7 +211,7 @@ public class HttpRestController {
 		}
 	}
 	
-	@RequestMapping(value = "/beansandthreads", produces = "text/html")
+	@RequestMapping(value = "/beansandthreads", produces = "text/html", method = RequestMethod.GET)
 	public String statsRoute() {
 		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
 		List<String> threadList = new ArrayList<String>();
